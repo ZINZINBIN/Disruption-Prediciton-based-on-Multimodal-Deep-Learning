e388a1cdcba10864f5a80b5943dda090aaa67b1a
zinzinbin <asdwlstn@naver.com>
2022-09-05 20:27:46 +0900

    update 0D data disruption prediction

--- src/generate_numerical_data.py
+++ src/generate_numerical_data.py
@@ -1,3 +1,4 @@
+from re import T
 import pandas as pd
 import numpy as np
 import scipy as sp
@@ -5,19 +6,21 @@ from typing import List, Tuple
 from scipy.interpolate import interp1d
 from tqdm.auto import tqdm
 
-def ts_interpolate(df : pd.DataFrame, cols : List, df_disruption : pd.DataFrame):
+def ts_interpolate(df : pd.DataFrame, cols : List, df_disruption : pd.DataFrame, dt : float = 1.0 / 210):
     
     df_interpolate = pd.DataFrame()
-    shot_list = np.unique(df.shot.values).tolist()
+    shot_list = np.unique(df_disruption.shot.values).tolist()
 
     for shot in tqdm(shot_list):
+        if shot not in df.shot.values:
+            continue
+
         # ts data with shot number = shot
         df_shot = df[df.shot == shot]
         dict_extend = {}
         t = df_shot.time.values.reshape(-1,)
 
         t_start = 0
-        dt = 1.0 / 210
         t_end = max(t)
 
         # quench info
@@ -28,7 +31,7 @@ def ts_interpolate(df : pd.DataFrame, cols : List, df_disruption : pd.DataFrame)
         if t_end < tTQend:
             t_end = tTQend + dt
 
-        t_extend = np.arange(t_start, t_end + dt, 1.0 / 210)
+        t_extend = np.arange(t_start, t_end + dt, dt)
         dict_extend['time'] = t_extend
         dict_extend['shot'] = [shot for _ in range(len(t_extend))]
 
@@ -36,23 +39,21 @@ def ts_interpolate(df : pd.DataFrame, cols : List, df_disruption : pd.DataFrame)
             data = df_shot[col].values.reshape(-1,)
             interp = interp1d(t, data, fill_value = 'extrapolate')
             data_extend = interp(t_extend).reshape(-1,)
-
             dict_extend[col] = data_extend
 
         df_shot_extend = pd.DataFrame(data = dict_extend)
-        df_interpolate = pd.concat([df_interpolate, df_shot_extend], axis = 0)
+        df_interpolate = pd.concat([df_interpolate, df_shot_extend], axis = 0).reset_index(drop = True)
 
     return df_interpolate
 
 if __name__ == "__main__":
     df = pd.read_csv("./dataset/KSTAR_Disruption_ts_data.csv")
-    df_disrupt = pd.read_csv("./dataset/KSTAR_Disruption_Shot_List.csv")
+    df_disrupt = pd.read_csv("./dataset/KSTAR_Disruption_Shot_List_extend.csv", encoding = "euc-kr")
     
     cols = df.columns[df.notna().any()].drop(['Unnamed: 0','shot','time']).tolist()
-
-    df_extend = ts_interpolate(df, cols, df_disrupt)
-
     fps = 210
-    df_extend['frame_idx'] = df_extend.time.apply(lambda x : int(round(x * fps)))
+    dt = 1.0 / fps * 4
 
-    df_extend.to_csv("./dataset/KSTAR_Disruption_ts_data_extend.csv")
+    df_extend = ts_interpolate(df, cols, df_disrupt, dt)
+    df_extend['frame_idx'] = df_extend.time.apply(lambda x : int(round(x * fps)))
+    df_extend.to_csv("./dataset/KSTAR_Disruption_ts_data_extend.csv", index = False)
